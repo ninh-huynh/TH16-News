28cecc3740e71d249b4caf45a5d69f4b751c8745
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-04-21 13:47:28 +0700

    add article page

---
+++ scripts/tieu-de-bai-viet.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+$(function () {
+    $('#header').load('./reuse-html/header.html');
+    $('#category-menu').load('./reuse-html/menu-bar.html', () => {
+       
+    });
+    $('#footer').load('./reuse-html/footer.html');
+});
