145f2b2729ccc846dae7637608d334e0190edddd
anhphuphan <anhphuphan29@gmail.com>
2019-04-23 16:03:28 +0700

    fix footer

--- scripts/danh-sach-bai-da-viet.js
+++ scripts/danh-sach-bai-da-viet.js
@@ -3,3 +3,10 @@ $(function () {
     $('#category-menu').load('./reuse-html/menu-bar.html');
     $('#footer').load('./reuse-html/footer.html');
 });
+
+
+// $(function() {
+//     var footerHeight = $('footer').height();
+
+//     $('body').css('margin-bottom', footerHeight + ' px');
+// });
