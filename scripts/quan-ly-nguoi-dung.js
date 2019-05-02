33e045e6ef4ed86915297aadfba52f39031cc643
anhphuphan <anhphuphan29@gmail.com>
2019-05-03 03:45:51 +0700

    update fixed table header

--- scripts/quan-ly-nguoi-dung.js
+++ scripts/quan-ly-nguoi-dung.js
@@ -49,32 +49,36 @@ function initTable() {
     detailView: true,
     detailFormatter: detailFormatter,
 
+    stickyHeader: true,
+    stickyHeaderOffsetY: 56,
+
     clickToSelect: true,
     
 
-    columns: [{field: 'state', checkbox: true, align: 'center', valign: 'middle', }, 
+    columns: [{field: 'state', checkbox: true, align: 'center', valign: 'middle',}, 
             { field: 'id', title: 'ID', align: 'center', valign: 'middle', sortable: true,  }, 
-            {  field: 'name', title: 'Tên', align: 'center',  valign: 'middle', sortable: true, }, 
+            {  field: 'name', title: 'Tên', align: 'center',  valign: 'middle', sortable: true, width: '40%'}, 
             { field: 'date', title: 'Ngày tạo', align: 'center', valign: 'middle', sortable: true,  }, 
-            { field: 'role', title: 'Vai trò', align: 'center', valign: 'center', }],
+            { field: 'role', title: 'Vai trò', align: 'center', valign: 'center', sortable: true, },
+            { field: 'category', title: 'Chuyên mục', align: 'center', valign: 'center', }],
         data: [{ "id": 1, "name": "Người dùng 1", "date": "1-1-2000", 'role': 'đọc giả',  },
             { "id": 2, "name": "Người dùng 2", "date": "1-1-1998", 'role': 'phóng viên', },
             { "id": 3, "name": "Người dùng 3", "date": "18-4-2015", 'role': 'đọc giả', },
             { "id": 4, "name": "Người dùng 4",  "date": "13-7-1986", 'role': 'đọc giả', },
-            { "id": 5, "name": "Người dùng 5",  "date": "18-9-1985", 'role': 'biên tập viên', },
+            { "id": 5, "name": "Người dùng 5",  "date": "18-9-1985", 'role': 'biên tập viên', 'category': 'Công nghệ'},
             { "id": 6, "name": "Người dùng 6",  "date": "15-2-2015", 'role': 'đọc giả', },
             { "id": 7, "name": "Người dùng 7",  "date": "5-1-1995", 'role': 'đọc giả', },
             { "id": 8, "name": "Người dùng 8",  "date": "1-2-1989", 'role': 'đọc giả', },
-            { "id": 9, "name": "Người dùng 9",  "date": "1-1-1992", 'role': 'biên tập viên', },
+            { "id": 9, "name": "Người dùng 9",  "date": "1-1-1992", 'role': 'biên tập viên', 'category': 'Thế giới'},
             { "id": 10, "name": "Người dùng 10",  "date": "2-8-1970", 'role': 'đọc giả', },
             { "id": 11, "name": "Người dùng 11",  "date": "1-1-2000", 'role': 'đọc giả', },
             { "id": 12, "name": "Người dùng 12",  "date": "15-1-1999", 'role': 'phóng viên', },
             { "id": 13, "name": "Người dùng 13",  "date": "1-52-2000", 'role': 'đọc giả', },
             { "id": 14, "name": "Người dùng 14",  "date": "11-10-1988", 'role': 'phóng viên', },
-            { "id": 15, "name": "Người dùng 15",  "date": "25-1-1991", 'role': 'biên tập viên', },
+            { "id": 15, "name": "Người dùng 15",  "date": "25-1-1991", 'role': 'biên tập viên', 'category': 'Sức khỏe'},
             { "id": 16, "name": "Người dùng 16",  "date": "7-3-1976", 'role': 'đọc giả', },
             { "id": 17, "name": "Người dùng 17",  "date": "17-8-2000", 'role': 'đọc giả', },
-            { "id": 18, "name": "Người dùng 18",  "date": "2-5-2000", 'role': 'biên tập viên', },
+            { "id": 18, "name": "Người dùng 18",  "date": "2-5-2000", 'role': 'biên tập viên', 'category': 'Giáo dục'},
             { "id": 19, "name": "Người dùng 19",  "date": "12-1-1998", 'role': 'đọc giả', },
             { "id": 20, "name": "Người dùng 20",  "date": "11-12-1999", 'role': 'đọc giả', }]
 
@@ -165,6 +169,8 @@ $('#context-menu').click( function() {
     $('#context-menu').hide();
 });
 
+
+/* Sidebar */
 $('#mySidenav a').mouseenter(function() {
     $(this).find('.sidenav-after-icon').hide();
     $(this).find('.sidenav-badge').show();
