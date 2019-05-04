4c42e375e47e824d7d4b7b3409fadc899044e84f
anhphuphan <anhphuphan29@gmail.com>
2019-05-04 16:26:26 +0700

    add quan-ly-nguoi-dung table filter

--- scripts/quan-ly-nguoi-dung.js
+++ scripts/quan-ly-nguoi-dung.js
@@ -1,4 +1,27 @@
 var $table = $('#table');
+var tableData = [{ "id": 1, "name": "Người dùng 1", "date": "1-1-2019", 'role': 'đọc giả', },
+            { "id": 2, "name": "Người dùng 2", "date": "1-1-2019", 'role': 'phóng viên', },
+            { "id": 3, "name": "Người dùng 3", "date": "18-4-2019", 'role': 'đọc giả', },
+            { "id": 4, "name": "Người dùng 4",  "date": "13-7-2019", 'role': 'đọc giả', },
+            { "id": 5, "name": "Người dùng 5",  "date": "18-9-2018", 'role': 'biên tập viên', 'category': 'Công nghệ'},
+            { "id": 6, "name": "Người dùng 6",  "date": "15-2-2019", 'role': 'đọc giả', },
+            { "id": 7, "name": "Người dùng 7",  "date": "5-1-2019", 'role': 'đọc giả', },
+            { "id": 8, "name": "Người dùng 8",  "date": "1-2-2018", 'role': 'đọc giả', },
+            { "id": 9, "name": "Người dùng 9",  "date": "4-5-2019", 'role': 'biên tập viên', 'category': 'Thế giới'},
+            { "id": 10, "name": "Người dùng 10",  "date": "2-8-2019", 'role': 'đọc giả', },
+            { "id": 11, "name": "Người dùng 11",  "date": "1-1-2019", 'role': 'đọc giả', },
+            { "id": 12, "name": "Người dùng 12",  "date": "15-1-2018", 'role': 'phóng viên', },
+            { "id": 13, "name": "Người dùng 13",  "date": "1-52-2019", 'role': 'đọc giả', },
+            { "id": 14, "name": "Người dùng 14",  "date": "11-10-2019", 'role': 'phóng viên', },
+            { "id": 15, "name": "Người dùng 15",  "date": "25-1-2018", 'role': 'biên tập viên', 'category': 'Sức khỏe'},
+            { "id": 16, "name": "Người dùng 16",  "date": "7-3-2019", 'role': 'đọc giả', },
+            { "id": 17, "name": "Người dùng 17",  "date": "17-8-2019", 'role': 'đọc giả', },
+            { "id": 18, "name": "Người dùng 18",  "date": "2-5-2019", 'role': 'biên tập viên', 'category': 'Giáo dục'},
+            { "id": 19, "name": "Người dùng 19",  "date": "12-1-2019", 'role': 'đọc giả', },
+            { "id": 20, "name": "Nguyễn Văn A",  "date": "6-6-2019", 'role': 'đọc giả', },
+            { "id": 21, "name": "Người dùng 21",  "date": "11-12-2018", 'role': 'đọc giả', }];
+const categories = ['thời sự', 'Thế giới', 'Kinh doanh', 'Công nghệ', 'Thể thao', 'Giải trí', 'Sức khỏe', 'Giáo dục'];
+
 
 $(function () {
     $('#header').load('./reuse-html/header.html');
@@ -13,8 +36,6 @@ $(function () {
 
     // table
     mounted();
-    $('.jumpto').find('.btn').prop('title', 'Đi').text('Đi');
-    $('.keep-open').find('.btn').prop('title', 'Columns');
 });
 
 function getIdSelections() {
@@ -23,19 +44,27 @@ function getIdSelections() {
     })
 } 
 
-function detailFormatter(index, row) {
-    var html = []
-    var header = $table.find('thead');
+function categoryFormatter(value, row, index, field) {
+    if (row.role.toUpperCase() === 'biên tập viên'.toUpperCase()) {
+        var html = [`
+            <select class='form-control category-dropdown-menu' style='text-align: center; text-align-last: center;' >
+        `];
 
-    $.each(row, function (key, value) {
-        if (key === 'state') {
-            return true;
+        for (var i = 0; i < categories.length; i++) {
+            html += '<option class="text-capitalize" value=' + categories[i];
+            if (value.toUpperCase() === categories[i].toUpperCase()) {
+                html += ' selected="selected"';
+            }
+            html += '>' + categories[i] + '</option>';
         }
+        html += '</select>';
+        return html;
+    }
+}
 
-      html.push('<p><b>' + header.find('[data-field=' + key + ']').text() + ':</b> ' + value + '</p>')
-    })
-    return html.join('')
-  }
+$('.category-dropdown-menu').on('click', function() {
+    console.log('test');
+})
 
 function initTable() {
     $table.bootstrapTable('destroy').bootstrapTable({
@@ -50,46 +79,24 @@ function initTable() {
     showColumns: true,
     showFullscreen: true,
     smartDisplay: true,
-    detailView: true,
-    detailFormatter: detailFormatter,
-
+    clickToSelect: true,
+    undefinedText: ' ',
     // extension
     stickyHeader: true,
     stickyHeaderOffsetY: 56,
     showJumpto: true,
     searchAccentNeutralise: true,
+    //filter
+    filterControl: true,
+    filterShowClear: true,
 
-    clickToSelect: true,
-    
-
-    columns: [{field: 'state', checkbox: true, align: 'center', valign: 'middle',}, 
-            { field: 'id', title: 'ID', align: 'center', valign: 'middle', sortable: true,  }, 
-            {  field: 'name', title: 'Tên', align: 'center',  valign: 'middle', sortable: true, width: '40%'}, 
-            { field: 'date', title: 'Ngày tạo', align: 'center', valign: 'middle', sortable: true,  }, 
-            { field: 'role', title: 'Vai trò', align: 'center', valign: 'center', sortable: true, },
-            { field: 'category', title: 'Chuyên mục', align: 'center', valign: 'center', }],
-        data: [{ "id": 1, "name": "Người dùng 1", "date": "1-1-2000", 'role': 'đọc giả',  },
-            { "id": 2, "name": "Người dùng 2", "date": "1-1-1998", 'role': 'phóng viên', },
-            { "id": 3, "name": "Người dùng 3", "date": "18-4-2015", 'role': 'đọc giả', },
-            { "id": 4, "name": "Người dùng 4",  "date": "13-7-1986", 'role': 'đọc giả', },
-            { "id": 5, "name": "Người dùng 5",  "date": "18-9-1985", 'role': 'biên tập viên', 'category': 'Công nghệ'},
-            { "id": 6, "name": "Người dùng 6",  "date": "15-2-2015", 'role': 'đọc giả', },
-            { "id": 7, "name": "Người dùng 7",  "date": "5-1-1995", 'role': 'đọc giả', },
-            { "id": 8, "name": "Người dùng 8",  "date": "1-2-1989", 'role': 'đọc giả', },
-            { "id": 9, "name": "Người dùng 9",  "date": "1-1-1992", 'role': 'biên tập viên', 'category': 'Thế giới'},
-            { "id": 10, "name": "Người dùng 10",  "date": "2-8-1970", 'role': 'đọc giả', },
-            { "id": 11, "name": "Người dùng 11",  "date": "1-1-2000", 'role': 'đọc giả', },
-            { "id": 12, "name": "Người dùng 12",  "date": "15-1-1999", 'role': 'phóng viên', },
-            { "id": 13, "name": "Người dùng 13",  "date": "1-52-2000", 'role': 'đọc giả', },
-            { "id": 14, "name": "Người dùng 14",  "date": "11-10-1988", 'role': 'phóng viên', },
-            { "id": 15, "name": "Người dùng 15",  "date": "25-1-1991", 'role': 'biên tập viên', 'category': 'Sức khỏe'},
-            { "id": 16, "name": "Người dùng 16",  "date": "7-3-1976", 'role': 'đọc giả', },
-            { "id": 17, "name": "Người dùng 17",  "date": "17-8-2000", 'role': 'đọc giả', },
-            { "id": 18, "name": "Người dùng 18",  "date": "2-5-2000", 'role': 'biên tập viên', 'category': 'Giáo dục'},
-            { "id": 19, "name": "Người dùng 19",  "date": "12-1-1998", 'role': 'đọc giả', },
-            { "id": 20, "name": "Nguyễn Văn A",  "date": "6-6-1999", 'role': 'đọc giả', },
-            { "id": 21, "name": "Người dùng 21",  "date": "11-12-1999", 'role': 'đọc giả', }]
-
+    columns: [{field: 'state', checkbox: true, align: 'center', valign: 'middle', width: '5%', }, 
+            { field: 'id', title: 'ID', align: 'center', valign: 'middle', sortable: true,  width: '5%'}, 
+            {  field: 'name', title: 'Tên', align: 'center',  valign: 'middle', sortable: true, width: '40%', }, 
+            { field: 'date', title: 'Ngày tạo', align: 'center', valign: 'middle', sortable: true, width: '20%', }, 
+            { field: 'role', title: 'Vai trò', align: 'center', valign: 'center', sortable: true, width: '15%', filterControl: 'select'},
+            { field: 'category', title: 'Chuyên mục', align: 'center', valign: 'center', filterControl: 'select', width: '15%', formatter: categoryFormatter}],
+    data: tableData,
         })
     }
 
@@ -105,17 +112,7 @@ function initTable() {
 })
 
 $table.on('all.bs.table', function (e, name, args) {
-    //console.log(name, args)
 })
-
-$table.on('click-row.bs.table', function(e, row, $element, field) {
-    var header = $table.find('thead');
-    console.log(header.find('[data-field="date"'));
-})
-
-// $table.on('dbl-click-row.bs.table', function(e, row, $element, field) {
-
-// });
 
 $('#remove').click(function () {
     var ids = getIdSelections()
@@ -124,6 +121,9 @@ $('#remove').click(function () {
         values: ids
     })
     $('#remove').prop('disabled', true)
+})
+
+$('#add').click(function() {
 })
           
 function mounted() {
@@ -159,7 +159,6 @@ $('#table').on('contextmenu', 'tr',  function(e) {
     e.preventDefault();
 
     // item click handler
-
     // remove event handler
     $('#removeItem').click( function(e) {
         $table.bootstrapTable('remove', {
@@ -176,7 +175,6 @@ $(document).click( function() {
 $('#context-menu').click( function() {
     $('#context-menu').hide();
 });
-
 
 /* Sidebar */
 $('#mySidenav a').mouseenter(function() {
