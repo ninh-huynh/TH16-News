1c9443e3c06aadc51975261ffb9c9e39f33a14ee
anhphuphan <anhphuphan29@gmail.com>
2019-05-06 18:14:04 +0700

    update manage-post, change context-menu item name, fix  on change article status event handle

--- HelloWorldNews/themes/the-logic/source/scripts/manage-post.js
+++ HelloWorldNews/themes/the-logic/source/scripts/manage-post.js
@@ -1,15 +1,15 @@
 var $table = $('#table');
 var tableData = [
-    {id: 1, title: {title: 'bài  viết 1', href: '#' }, author: 'tác giả 1', publish_date: "1-1-2019", status: 'Đã xuất bản'},
-    {id: 2, title: {title: 'bài  viết 2', href: '#' }, author: 'tác giả 2', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
-    {id: 3, title: {title: 'bài  viết 3', href: 'https://mp3.zing.vn' }, author: 'tác giả 3', publish_date: "3-3-2019", status: 'Đã xuất bản'},
-    {id: 4, title: {title: 'bài  viết 4', href: 'https://mp3.zing.vn' }, author: 'tác giả 4', publish_date: "4-4-2019", status: 'Đã xuất bản'},
-    {id: 5, title: {title: 'bài  viết 5', href: 'https://mp3.zing.vn' }, author: 'tác giả 5', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
-    {id: 6, title: {title: 'bài  viết 6', href: 'https://mp3.zing.vn' }, author: 'tác giả 6', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
-    {id: 7, title: {title: 'bài  viết 7', href: 'https://mp3.zing.vn' }, author: 'tác giả 7', publish_date: "7-7-2019", status: 'Đã xuất bản'},
-    {id: 8, title: {title: 'bài  viết 8', href: 'https://mp3.zing.vn' }, author: 'tác giả 8', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
-    {id: 9, title: {title: 'bài  viết 9', href: 'https://mp3.zing.vn' }, author: 'tác giả 9', publish_date: "9-9-2019", status: 'Đã xuất bản'},
-    {id: 10, title: {title: 'bài  viết 10', href: 'https://mp3.zing.vn' }, author: 'tác giả 10', publish_date: "10-10-2019", status: 'Đã xuất bản'}
+    {id: 1, article: {title: 'bài  viết 1', href: '#' }, author: 'tác giả 1', publish_date: "1-1-2019", status: 'Đã xuất bản'},
+    {id: 2, article: {title: 'bài  viết 2', href: '#' }, author: 'tác giả 2', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
+    {id: 3, article: {title: 'bài  viết 3', href: 'https://mp3.zing.vn' }, author: 'tác giả 3', publish_date: "3-3-2019", status: 'Đã xuất bản'},
+    {id: 4, article: {title: 'bài  viết 4', href: 'https://mp3.zing.vn' }, author: 'tác giả 4', publish_date: "4-4-2019", status: 'Đã xuất bản'},
+    {id: 5, article: {title: 'bài  viết 5', href: 'https://mp3.zing.vn' }, author: 'tác giả 5', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
+    {id: 6, article: {title: 'bài  viết 6', href: 'https://mp3.zing.vn' }, author: 'tác giả 6', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
+    {id: 7, article: {title: 'bài  viết 7', href: 'https://mp3.zing.vn' }, author: 'tác giả 7', publish_date: "7-7-2019", status: 'Đã xuất bản'},
+    {id: 8, article: {title: 'bài  viết 8', href: 'https://mp3.zing.vn' }, author: 'tác giả 8', publish_date: "", status: 'Đã được duyệt & chờ xuất bản'},
+    {id: 9, article: {title: 'bài  viết 9', href: 'https://mp3.zing.vn' }, author: 'tác giả 9', publish_date: "9-9-2019", status: 'Đã xuất bản'},
+    {id: 10, article: {title: 'bài  viết 10', href: 'https://mp3.zing.vn' }, author: 'tác giả 10', publish_date: "10-10-2019", status: 'Đã xuất bản'}
 ];
 
 var statuses = ['đã xuất bản', 'đã được duyệt & chờ xuất bản'];
@@ -32,7 +32,7 @@ function getIdSelections() {
     })
 }
 
-function titleFormatter(value, row, index) {
+function articleFormatter(value, row, index) {
     return [
         '<a href="' + value.href + '" target="_blank">' + value.title.substr(0, 1).toUpperCase() + value.title.substr(1) + '</a>'
     ].join('');
@@ -67,7 +67,8 @@ function statusFormatter(value, row, index) {
 
 window.operateEvents = {
     'change .status-dropdown-menu' : function (e, value, row, index) {
-        $table.bootstrapTable('updateCellById', {id: index + 1, field: 'status', value: $('tbody tr[data-index="' + index + '"]').find('select option:selected').text()})
+        var id = $table.bootstrapTable('getData', true)[index].id;
+        $table.bootstrapTable('updateCellById', {id: id, field: 'status', value: $('tbody tr[data-index="' + index + '"]').find('select option:selected').text()})
     }
 }
 
@@ -96,7 +97,7 @@ function initTable() {
 
     columns: [{field: 'state', checkbox: true, align: 'center', valign: 'middle', width: '5%', }, 
             { field: 'id', title: 'ID', align: 'center', valign: 'middle', sortable: true,  width: '5%'}, 
-            {  field: 'title', title: 'Bài viết', align: 'center',  valign: 'middle', formatter: titleFormatter, sortable: true, },
+            {  field: 'article', title: 'Bài viết', align: 'center',  valign: 'middle', formatter: articleFormatter, sortable: true, },
             {  field: 'author', title: 'Tác giả', align: 'center',  valign: 'middle', width: '20%', formatter: nameFormatter, sortable: true, },
             {  field: 'publish_date', title: 'Ngày xuất bản', align: 'center',  valign: 'middle', width: '15%', sortable: true, },
             {  field: 'status', title: 'Trạng thái', align: 'center',  valign: 'middle', width: '20%', formatter: statusFormatter, events: window.operateEvents, sortable: true, }],
