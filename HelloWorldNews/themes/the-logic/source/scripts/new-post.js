76c50aac91eb1278bf58e1feeb54d9741ee63758
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-05-06 16:07:19 +0700

    validate sign-in form, sign-up form

--- HelloWorldNews/themes/the-logic/source/scripts/new-post.js
+++ HelloWorldNews/themes/the-logic/source/scripts/new-post.js
@@ -74,25 +74,7 @@ $('#selectCategory').change(function(event) {
     
 });
 
-$(function() {
-    'use strict';
-    
-    window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            
-        form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-        });
-    }, false);
-});
+
 
 // make sure CKEditor toolbar could not be overlapped by navbar
 var myEditor;
