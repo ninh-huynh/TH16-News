21fa2a59cd1f96918f8e21eb5c0c4c44b66397e0
anhphuphan <anhphuphan29@gmail.com>
2019-05-08 20:41:06 +0700

    remove unuse html, css, js files

--- zPhase01/scripts/the-gioi.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-$(function () {
-    $('#header').load('./reuse-html/header.html');
-    $('#category-menu').load('./reuse-html/menu-bar.html', () => {
-        $('#the-gioi').addClass('active');
-    });
-    $('#footer').load('./reuse-html/footer.html');
-});
