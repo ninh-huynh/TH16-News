0fbd5978204bb29aadb7c94b62ee5091863173fc
anhphuphan <anhphuphan29@gmail.com>
2019-05-08 15:50:25 +0700

    deploy

---
+++ scripts/danh-sach-bai-da-viet.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('#header').load('./reuse-html/header.html');
+    $('#category-menu').load('./reuse-html/menu-bar.html');
+    $('#footer').load('./reuse-html/footer.html');
+});
+
+
+// $(function() {
+//     var footerHeight = $('footer').height();
+
+//     $('body').css('margin-bottom', footerHeight + ' px');
+// });
