037861ee6b542bdddf0c7e8bc5af15c20576186b
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-05-19 23:15:24 +0700

    complete basic db api

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,97 @@
 var mysql = require('mysql');
 
-function createConnection() {
-    return mysql.createConnection({
-        host: '13.67.104.190',
-        user: 'guest',
-        password: '&JE#ZE*0B*3t',
-        database: 'th16_news'
-    });
-}
+//TODO: Change the user name and password for specific role (guest, admin, ...)
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: '13.67.104.190',
+    user: 'admin',              
+    password: 'R&ci!oD$WuCe',
+    database: 'th16_news'
+});
 
 module.exports = {
+
+    // For testing purpose only, will remove it from the future
     testConnection: () => {
         return new Promise((resolve, reject) => {
-            var connection = createConnection();
-            connection.connect((error) => {
-                if (error)
-                    reject(error);
-                else
+            pool.getConnection((err, connection) => {
+                if (err)
+                    reject(err);
+                else {
                     resolve('Connect to DB Server successfully!');
+                    connection.release();
+                }
             });
-            connection.end();
         });
     },
-    
+
+    // Retrieving rows from the table
+    // sql: query string
+
+    // Ex: sql = 'SELECT * FROM category'
     load: sql => {
         return new Promise((resolve, reject) => {
-            var connection = createConnection();
-            connection.connect();
-            connection.query(sql, (err, results, fiels) => {
+            // var pool = createPool();
+            pool.query(sql, (err, results, fields) => {
                 if (err)
                     reject(err);
                 else
                     resolve(results);
             });
-            connection.end();
+        });
+    },
+
+    // Add an entity (js object) to the table name `table`
+    // enitity: javascript object (the property name must be same as the table column name)
+    // table: table name
+
+    //Ex: entity = {name: 'cat1', id: 1, parentId: null}
+    //    table = 'category'
+    add: (entity, table) => {
+        return new Promise((resolve, reject) => {
+            pool.query(`INSERT INTO ${table} SET ? `, entity, (err, results, fields) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(results.insertId);
+            });
+        });
+    },
+
+    // Update a row in the table name `table`
+    // enitity: javascript object (the property name must be same as the table column name)
+    // table: table name
+
+    //Ex: entity = {name: 'cat1', id: 1, parentId: null}
+    //    table = 'category'
+    update: (entity, table) => {
+        return new Promise((resolve, reject) => {
+            var id = entity.id;
+            delete entity['id'];
+            pool.query(`UPDATE ${table} SET ? WHERE id = ? `, [entity, id], (err, results, fields) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(results.changedRows);
+            });
+        });
+    },
+
+    // Delete a row in the table name `table`
+    // enitity: javascript object (the property name must be same as the table column name)
+    // table: table name
+
+    //Ex: entity = {name: 'cat1', id: 1, parentId: null}
+    //    table = 'category'
+    remove: (entity, table) => {
+        return new Promise((resolve, reject) => {
+            var id = entity.id;
+            pool.query(`DELETE FROM ${table} WHERE id = ${id} `, (err, results, fields) => {
+                if (err)
+                    reject(err);
+                else
+                    resolve(results.affectedRows);
+            });
         });
     }
 };
