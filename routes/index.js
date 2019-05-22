0eb97fb8822a4fca04239a2cba43d5e837792233
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-05-22 09:55:19 +0700

    load newest article dynamicly

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Promise.all([
-        articles.loadWeeklyTrend(5, 0)])
-        .then(([weeklyTrendRows]) => {
-            var obj = { articles: weeklyTrendRows };
+        articles.loadWeeklyTrend(5, 0),
+        articles.loadNewest(10, 0)])
+        .then(([weeklyTrendRows, newestRows]) => {
+            var obj = {
+                articles: weeklyTrendRows,
+                newestArticles: newestRows
+            };
             res.render('index', obj);
         })
         .catch(next);
