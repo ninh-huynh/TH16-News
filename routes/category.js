dedbeee88261a19288220c770bde65cbc505bd73
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-06-05 16:31:55 +0700

    update paginator in category and search

--- routes/category.js
+++ routes/category.js
@@ -2,10 +2,9 @@ var express = require('express');
 var route = express.Router();
 var article = require('../models/articles');
 var categories = require('../models/categories');
+var paginator = require('../utils/paginator');
 
 const articlePerPage = 5;       // define favorite number here.
-var totalArticle;               // get from database
-var totalPage;                  // calculate later
 
 route.get('/:name', (req, res, next) => {
     var categoryName = req.params.name;
@@ -20,35 +19,25 @@ route.get('/:name', (req, res, next) => {
         if (current === undefined)
             next(new Error('404 not found'));
     }
-
-    article.countTotalByCategory_public(categoryPath)
-        .then(total => {
-            totalArticle = total;
-            totalPage = Math.floor(totalArticle / articlePerPage);
-
-            if (totalArticle % articlePerPage != 0) { totalPage++; }
-
-            var page = {
-                current: current,
-                total: totalPage,
-                next: current + 1,
-                prev: current - 1
+    
+    var category;
+    var page;
+    categories.loadByLink(categoryPath)
+        .then(categoryEntity => {
+            category = categoryEntity;
+            return article.countTotalByCategory(category);
+        })
+        .then(totalArticle => {
+            page = paginator.get(current, totalArticle, articlePerPage);
+            return article.loadByCategoryEntity(category, articlePerPage, (current - 1) * articlePerPage);
+        })
+        .then(articles => {
+            var obj = {
+                articles: articles,
+                category,
+                page: page
             };
-
-            if (current === 1) { page.prev = 0; }
-            if (current === totalPage) { page.next = 0; }
-
-            Promise.all([categories.loadByLink(categoryPath),
-                article.loadByCategoryLink(categoryPath, articlePerPage, (current - 1) * articlePerPage)])
-                .then(([categoryEntity, articles]) => {
-                    var obj = {
-                        articles: articles,
-                        category: categoryEntity,
-                        page: page
-                    };
-                    res.render('category', obj);
-                })
-                .catch(next);
+            res.render('category', obj);
         })
         .catch(next);
 });
--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,9 @@ var express = require('express');
 var articleModel = require('../models/articles');
 var router = express.Router();
 var tags = require('../models/tags');
+var paginator = require('../utils/paginator');
 
 const articlePerPage = 5;       // define favorite number here.
-var totalArticle;               // get from database
-var totalPage;                  // calculate later
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -22,33 +20,19 @@ router.get('/', function (req, res, next) {
             next(new Error('404 not found'));
     }
 
+    var page;
     articleModel.countTotalByTitle(keyword)
-        .then(total => {
-            totalArticle = total;
-            totalPage = Math.floor(totalArticle / articlePerPage);
-
-            if (totalArticle % articlePerPage != 0) { totalPage++; }
-
-            var page = {
-                current: current,
-                total: totalPage,
-                next: current + 1,
-                prev: current - 1
+        .then(totalArticle => {
+            page = paginator.get(current, totalArticle, articlePerPage);
+            return articleModel.searchByKeyword(keyword, articlePerPage, (current - 1) * articlePerPage);
+        })
+        .then(rows => {
+            var obj = {
+                keyword: keyword,
+                articles: rows,
+                page: page,
             };
-
-            if (current === 1) { page.prev = 0; }
-            if (current === totalPage) { page.next = 0; }
-        
-            articleModel.searchByTitle(keyword, articlePerPage, (current - 1) * articlePerPage)
-                .then(rows => {
-                    var obj = {
-                        keyword: keyword,
-                        articles: rows,
-                        page: page,
-                    };
-                    res.render('search', obj);
-                })
-                .catch(next);
+            res.render('search', obj);
         })
         .catch(next);
 });
