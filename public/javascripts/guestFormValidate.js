8bc45db14990a69930cacc1bd990b99d50793f07
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-06-11 08:10:33 +0700

    update signup form validator

--- public/javascripts/guestFormValidate.js
+++ public/javascripts/guestFormValidate.js
@@ -30,8 +30,17 @@ $(function () {
                 required: 'Nhập mật khẩu của bạn'
             }
         },
-        errorClass: 'invalid',
-        validClass: 'valid',
+        errorElement: 'em',
+
+        errorPlacement: function (error, element) {
+            // Add the `invalid-feedback` class to the error element
+            error.addClass('invalid-feedback');
+            if (element.prop('type') === 'checkbox') {
+                error.insertAfter(element.next('label'));
+            } else {
+                error.insertAfter(element);
+            }
+        },
         highlight: function (element, errorClass, validClass) {
             $(element).addClass('is-invalid').removeClass('is-valid');
         },
@@ -64,8 +73,17 @@ $(function () {
                 minlength: 'Mật khẩu phải có độ dài từ 5 kí tự'
             }
         },
-        errorClass: 'invalid',
-        validClass: 'valid',
+        errorElement: 'em',
+
+        errorPlacement: function (error, element) {
+            // Add the `invalid-feedback` class to the error element
+            error.addClass('invalid-feedback');
+            if (element.prop('type') === 'checkbox') {
+                error.insertAfter(element.next('label'));
+            } else {
+                error.insertAfter(element);
+            }
+        },
         highlight: function (element, errorClass, validClass) {
             $(element).addClass('is-invalid').removeClass('is-valid');
         },
