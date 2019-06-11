745a9382146efb05c1928aa098464fff756cf7d0
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-06-11 11:58:59 +0700

    update menu category

--- middleware/menu_bar.js
+++ middleware/menu_bar.js
@@ -3,6 +3,14 @@ var router = express.Router();
 var category = require('../models/categories');
 
 router.use((req, res, next) => {
+    var url = req.url;
+    if (url.match('/admin/*') ||
+        url.match('/editor/*') ||
+        url.match('/writer/*')) {
+        next();
+        return;
+    }
+    
     category.loadMainIncludeChild()
         .then(rows => {
             //Đối tượng res.locals lưu giữ các biến local trong 1 request,
