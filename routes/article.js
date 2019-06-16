19afabebff1b59ae8df154466c4bfcce655c1459
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-06-16 14:18:26 +0700

    check user seassion, fill user name in comment section

--- routes/article.js
+++ routes/article.js
@@ -3,18 +3,37 @@ var router = express.Router();
 var articleModel = require('../models/articles');
 var commentModel = require('../models/comments');
 var articleViewsModel = require('../models/article_views');
+var userRoleModel = require('../models/user_role');
 var moment = require('moment');
 
 
 router.get('/:title', (req, res, next) => {
+    var userEntity = req.user;
     var title = req.params.title;
     var articleEntity;
     var today = moment().format('YYYY-MM-DD');
-    var isAbleToView;   
+    var isAbleToView;
     var isSubscriberCanViewPremium = false;     // check the subscriber login and currentDate <= expiry date
+    var subscriberName = null;
 
     title = title.replace(/-/g, ' ');
-    articleModel.searchByTitle(title)
+
+    userRoleModel.getId('Độc giả')
+        .then(roleID => {
+
+
+            if (userEntity !== undefined && userEntity.roleID === roleID) {
+
+                if (userEntity.roleID === roleID) {
+                    subscriberName = userEntity.name;
+                }
+
+                if (moment().isBefore(userEntity.expiryDate))
+                    isSubscriberCanViewPremium = true;
+            }
+
+            return articleModel.searchByTitle(title);
+        })
         .then(article => {
             articleEntity = article;
             var pLoadBySameCategory = articleModel.loadBySameCategory(articleEntity);
@@ -27,7 +46,8 @@ router.get('/:title', (req, res, next) => {
             var obj = {
                 article: articleEntity,
                 sameCategoryArticle: rows,
-                isAbleToView
+                isAbleToView,
+                subscriberName
             };
             res.render('post', obj);
         })
@@ -37,7 +57,7 @@ router.get('/:title', (req, res, next) => {
 router.post('/:title/comment', (req, res, next) => {
     var title = req.body.articleTitle;
     title = title.replace(/-/g, ' ');
-    
+
     articleModel.searchByTitle(title)
         .then(article => {
             delete req.body.articleTitle;
